52af3b351c3d5435fd3b43ef916a02e010758d26
Shiwei Ge <geshiwei@amazon.com>
2024-06-24 10:40:24 -0700

    2nd commit

--- init.lua
+++ init.lua
@@ -62,8 +62,14 @@ require("toggleterm").setup {
   },
 }
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 -- Map ]t to go to the next tab
 vim.api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { noremap = true, silent = true })
 
 >>>>>>> a8719e5 (2nd commit)
+=======
+-- Map ]t to go to the next tab
+vim.api.nvim_set_keymap('n', ']t', ':tabnext<CR>', { noremap = true, silent = true })
+
+>>>>>>> 3d1c7bf (2nd commit)
