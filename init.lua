00c2166ef61f97e034682a344d6c77d318f8d7e7
Shiwei Ge <geshiwei@amazon.com>
2024-10-15 21:04:57 -0700

    telescope lsp

--- init.lua
+++ init.lua
@@ -38,15 +38,3 @@ vim.schedule(function()
   require "mappings"
 end)
 
--- local telescope = require "telescope"
---
--- telescope.setup {
---   pickers = {
---     live_grep = {
---       additional_args = function(opts)
---         return { "--ignore-case" }
---       end,
---     },
---   },
--- }
-
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,17 +1,37 @@
--- EXAMPLE
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
-
+local nvlsp = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "eslint", "ts_ls", "lua_ls", "tailwindcss" }
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+-- Custom on_attach function
+local custom_on_attach = function(client, bufnr)
+  -- Call NvChad's default on_attach
+  nvlsp.on_attach(client, bufnr)
+
+  -- Helper function to set key mappings
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+
+  -- Telescope LSP key mappings
+  local opts = { noremap = true, silent = true }
+  buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+  buf_set_keymap('n', 'gs', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+  buf_set_keymap('n', 'gS', '<cmd>Telescope lsp_workspace_symbols<CR>', opts)
+  buf_set_keymap('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts)
+  buf_set_keymap('n', 'gh', '<cmd>Telescope lsp_diagostics<CR>', opts)
+  -- Add more mappings as needed
 end
 
+-- Load NvChad's default configurations
+nvlsp.defaults()
+
+-- Set up LSP servers with the custom on_attach
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = custom_on_attach,  -- Use custom on_attach
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
