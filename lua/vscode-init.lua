4d58a318042ce6edb6d74056a44581581ac87670
Shiwei Ge <geshiwei@amazon.com>
2025-05-28 14:07:37 -0700

    vscode neovim config

--- lua/vscode-init.lua
+++ lua/vscode-init.lua
@@ -1,6 +1,259 @@
 -- VSCode Neovim Integration
 local M = {}
 
+local whichkey = {
+    show = function()
+        vim.fn.VSCodeNotify("whichkey.show")
+    end
+}
+
+local comment = {
+    selected = function()
+        vim.fn.VSCodeNotifyRange("editor.action.commentLine", vim.fn.line("v"), vim.fn.line("."), 1)
+    end
+}
+
+local file = {
+    new = function()
+        vim.fn.VSCodeNotify("workbench.explorer.fileView.focus")
+        vim.fn.VSCodeNotify("explorer.newFile")
+    end,
+
+    save = function()
+        vim.fn.VSCodeNotify("workbench.action.files.save")
+    end,
+
+    saveAll = function()
+        vim.fn.VSCodeNotify("workbench.action.files.saveAll")
+    end,
+
+    format = function()
+        vim.fn.VSCodeNotify("editor.action.formatDocument")
+    end,
+
+    showInExplorer = function()
+        vim.fn.VSCodeNotify("workbench.files.action.showActiveFileInExplorer")
+    end,
+
+    rename = function()
+        vim.fn.VSCodeNotify("workbench.files.action.showActiveFileInExplorer")
+        vim.fn.VSCodeNotify("renameFile")
+    end
+}
+
+local error = {
+    list = function()
+        vim.fn.VSCodeNotify("workbench.actions.view.problems")
+    end,
+    next = function()
+        vim.fn.VSCodeNotify("editor.action.marker.next")
+    end,
+    previous = function()
+        vim.fn.VSCodeNotify("editor.action.marker.prev")
+    end
+}
+
+local editor = {
+    closeActive = function()
+        vim.fn.VSCodeNotify("workbench.action.closeActiveEditor")
+    end,
+
+    closeOther = function()
+        vim.fn.VSCodeNotify("workbench.action.closeOtherEditors")
+    end,
+
+    organizeImport = function()
+        vim.fn.VSCodeNotify("editor.action.organizeImports")
+    end
+}
+
+local workbench = {
+    showCommands = function()
+        vim.fn.VSCodeNotify("workbench.action.showCommands")
+    end,
+    previousEditor = function()
+        vim.fn.VSCodeNotify("workbench.action.previousEditor")
+    end,
+    nextEditor = function()
+        vim.fn.VSCodeNotify("workbench.action.nextEditor")
+    end
+}
+
+local toggle = {
+    toggleActivityBar = function()
+        vim.fn.VSCodeNotify("workbench.action.toggleActivityBarVisibility")
+    end,
+    toggleSideBarVisibility = function()
+        vim.fn.VSCodeNotify("workbench.action.toggleSidebarVisibility")
+    end,
+    toggleZenMode = function()
+        vim.fn.VSCodeNotify("workbench.action.toggleZenMode")
+    end,
+    theme = function()
+        vim.fn.VSCodeNotify("workbench.action.selectTheme")
+    end
+}
+
+local symbol = {
+    rename = function()
+        vim.fn.VSCodeNotify("editor.action.rename")
+    end
+}
+
+-- if bookmark extension is used
+local bookmark = {
+    toggle = function()
+        vim.fn.VSCodeNotify("bookmarks.toggle")
+    end,
+    list = function()
+        vim.fn.VSCodeNotify("bookmarks.list")
+    end,
+    previous = function()
+        vim.fn.VSCodeNotify("bookmarks.jumpToPrevious")
+    end,
+    next = function()
+        vim.fn.VSCodeNotify("bookmarks.jumpToNext")
+    end
+}
+
+local search = {
+    reference = function()
+        vim.fn.VSCodeNotify("editor.action.referenceSearch.trigger")
+    end,
+    referenceInSideBar = function()
+        vim.fn.VSCodeNotify("references-view.find")
+    end,
+    project = function()
+        vim.fn.VSCodeNotify("editor.action.addSelectionToNextFindMatch")
+        vim.fn.VSCodeNotify("workbench.action.findInFiles")
+    end,
+    text = function()
+        vim.fn.VSCodeNotify("workbench.action.findInFiles")
+    end
+}
+
+local project = {
+    findFile = function()
+        vim.fn.VSCodeNotify("workbench.action.quickOpen")
+    end,
+    switch = function()
+        vim.fn.VSCodeNotify("workbench.action.openRecent")
+    end,
+    tree = function()
+        vim.fn.VSCodeNotify("workbench.view.explorer")
+    end
+}
+
+local git = {
+    init = function()
+        vim.fn.VSCodeNotify("git.init")
+    end,
+    status = function()
+        vim.fn.VSCodeNotify("workbench.view.scm")
+    end,
+    switch = function()
+        vim.fn.VSCodeNotify("git.checkout")
+    end,
+    deleteBranch = function()
+        vim.fn.VSCodeNotify("git.deleteBranch")
+    end,
+    push = function()
+        vim.fn.VSCodeNotify("git.push")
+    end,
+    pull = function()
+        vim.fn.VSCodeNotify("git.pull")
+    end,
+    fetch = function()
+        vim.fn.VSCodeNotify("git.fetch")
+    end,
+    commit = function()
+        vim.fn.VSCodeNotify("git.commit")
+    end,
+    publish = function()
+        vim.fn.VSCodeNotify("git.publish")
+    end,
+
+    -- if gitlens installed
+    graph = function()
+        vim.fn.VSCodeNotify("gitlens.showGraphPage")
+    end
+}
+
+local fold = {
+    toggle = function()
+        vim.fn.VSCodeNotify("editor.toggleFold")
+    end,
+
+    all = function()
+        vim.fn.VSCodeNotify("editor.foldAll")
+    end,
+    openAll = function()
+        vim.fn.VSCodeNotify("editor.unfoldAll")
+    end,
+
+    close = function()
+        vim.fn.VSCodeNotify("editor.fold")
+    end,
+    open = function()
+        vim.fn.VSCodeNotify("editor.unfold")
+    end,
+    openRecursive = function()
+        vim.fn.VSCodeNotify("editor.unfoldRecursively")
+    end,
+
+    blockComment = function()
+        vim.fn.VSCodeNotify("editor.foldAllBlockComments")
+    end,
+
+    allMarkerRegion = function()
+        vim.fn.VSCodeNotify("editor.foldAllMarkerRegions")
+    end,
+    openAllMarkerRegion = function()
+        vim.fn.VSCodeNotify("editor.unfoldAllMarkerRegions")
+    end
+}
+
+local vscode = {
+    focusEditor = function()
+        vim.fn.VSCodeNotify("workbench.action.focusActiveEditorGroup")
+    end,
+    moveSideBarRight = function()
+        vim.fn.VSCodeNotify("workbench.action.moveSideBarRight")
+    end,
+    moveSideBarLeft = function()
+        vim.fn.VSCodeNotify("workbench.action.moveSideBarLeft")
+    end
+}
+
+local refactor = {
+    showMenu = function()
+        vim.fn.VSCodeNotify("editor.action.refactor")
+    end
+}
+
+-- https://vi.stackexchange.com/a/31887
+local nv_keymap = function(lhs, rhs)
+    vim.api.nvim_set_keymap('n', lhs, rhs, {
+        noremap = true,
+        silent = true
+    })
+    vim.api.nvim_set_keymap('v', lhs, rhs, {
+        noremap = true,
+        silent = true
+    })
+end
+
+local nx_keymap = function(lhs, rhs)
+    vim.api.nvim_set_keymap('n', lhs, rhs, {
+        silent = true
+    })
+    vim.api.nvim_set_keymap('v', lhs, rhs, {
+        silent = true
+    })
+end
+
+-- #region keymap
+vim.g.mapleader = " "
 -- Detect if running inside VSCode
 M.is_vscode = function()
     return vim.g.vscode ~= nil
@@ -25,16 +278,18 @@ M.setup = function()
     -- VSCode-specific keymappings based on your existing preferences
     local map = vim.keymap.set
 
-    map("n", "<Space>", "", opts)
+    -- map("n", "<Space>", "", opts)
     -- Your existing basic mappings
+    vim.keymap.set({'n', 'v'}, "<leader>", whichkey.show)
+    vim.keymap.set({'n', 'v'}, "<leader>/", comment.selected)
     map("n", ";", ":", {
         desc = "CMD enter command mode"
     })
-    
+
     -- Command palette
-    map("n", "<leader><leader>", "<cmd>call VSCodeNotify('workbench.action.showCommands')<CR>", {
-        desc = "Open command palette"
-    })
+    -- map("n", "<leader><leader>", "<cmd>call VSCodeNotify('workbench.action.showCommands')<CR>", {
+    --     desc = "Open command palette"
+    -- })
     -- Your existing clipboard operations
     map({"n", "v"}, "<leader>y", '"+y')
     map({"n", "v"}, "<leader>Y", '"+Y')
@@ -56,7 +311,22 @@ M.setup = function()
     map("n", "<leader>cb", "<cmd>call VSCodeNotify('workbench.action.closeOtherEditors')<CR>", {
         desc = "close all buffers except for the current one"
     })
-    
+
+    -- bookmark
+    vim.keymap.set({'n'}, "<leader>m", bookmark.toggle)
+    vim.keymap.set({'n'}, "<leader>mt", bookmark.toggle)
+    vim.keymap.set({'n'}, "<leader>ml", bookmark.list)
+    vim.keymap.set({'n'}, "<leader>mn", bookmark.next)
+    vim.keymap.set({'n'}, "<leader>mp", bookmark.previous)
+
+    -- git
+    vim.keymap.set({'n'}, "<leader>gb", git.switch)
+    vim.keymap.set({'n'}, "<leader>gi", git.init)
+    vim.keymap.set({'n'}, "<leader>gd", git.deleteBranch)
+    vim.keymap.set({'n'}, "<leader>gf", git.fetch)
+    vim.keymap.set({'n'}, "<leader>gs", git.status)
+    vim.keymap.set({'n'}, "<leader>gp", git.pull)
+    vim.keymap.set({'n'}, "<leader>gg", git.graph)
     -- Close current buffer
     map("n", "<leader>x", "<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>", {
         desc = "Close current buffer"
@@ -87,8 +357,11 @@ M.setup = function()
     map("n", "gd", "<cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>", {
         desc = "Go to definition"
     })
-    map("n", "gr", "<cmd>call VSCodeNotify('editor.action.goToReferences')<CR>", {
+    map("n", "gr", "<cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>", {
         desc = "Go to references"
+    })
+    map("n", "gR", "<cmd>call VSCodeNotify('references-view.find')<CR>", {
+        desc = "Go to references in side bar"
     })
     map("n", "gi", "<cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>", {
         desc = "Go to implementation"
@@ -182,9 +455,9 @@ M.setup = function()
     map("n", "<leader>fo", "<cmd>call VSCodeNotify('editor.action.showOutgoingCalls')<CR>", {
         desc = "Outgoing calls"
     })
-    map("n", "<leader>/", "<cmd>call VSCodeNotify('editor.action.addCommentLine')<CR>", {
-        desc = "Outgoing calls"
-    })
+    -- map("n", "<leader>/", "<cmd>call VSCodeNotify('editor.action.addCommentLine')<CR>", {
+    --     desc = "Outgoing calls"
+    -- })
 
     -- Fix Escape key to exit search/find mode
     map("n", "<ESC>",
@@ -194,7 +467,7 @@ M.setup = function()
             silent = true
         })
     map("n", "<Esc>", "<Esc>:noh<CR>", opts)
-    
+
     -- Terminal mappings (additional)
     map({"n", "v"}, "<leader>tt", "<cmd>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>", {
         desc = "Toggle terminal panel"
@@ -208,7 +481,7 @@ M.setup = function()
     map({"n", "v"}, "<leader>tk", "<cmd>call VSCodeNotify('workbench.action.terminal.kill')<CR>", {
         desc = "Kill terminal instance"
     })
-    
+
     -- Fold mappings
     map("n", "za", "<cmd>call VSCodeNotify('editor.toggleFold')<CR>", {
         desc = "Toggle fold under cursor"
