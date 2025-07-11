4f2a68d9f36a4bbead66cad12f2ced27b4fbe5c3
Shiwei Ge <geshiwei@amazon.com>
2025-07-11 22:32:49 +0000

    use new nvchad

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,6 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
+-- EXAMPLE
 local servers = { "html", "cssls", "ts_ls", "eslint", "tailwindcss", 'pylsp', 'golangci_lint_ls', 'gopls'}
 vim.lsp.enable(servers)
